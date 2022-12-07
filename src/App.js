0859f0d457de174382f7cb8fae24da3fe044a9f0
Mariusz Rojewski <rojewski@webkon.eu>
2022-12-07 12:03:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Działa</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MyInput.js
@@ -0,0 +1,40 @@
+import { PropTypes } from "prop-types";
+import React from "react";
+
+const MyInput = (props) => {
+  const { type, label, name, value, className, onChange, error } = props;
+  console.log(error);
+
+  return (
+    <div>
+      <label htmlFor={name}>
+        {label} {type !== "submit" ? ":" : ""}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        className={className}
+        onChange={onChange}
+      />
+      {error && (
+        <small className="alert alert-danger form-text text-muted">
+          {error}
+        </small>
+      )}
+    </div>
+  );
+};
+
+MyInput.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  // value: PropTypes.string,
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  error: PropTypes.string,
+};
+
+export default MyInput;
